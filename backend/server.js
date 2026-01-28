e441ceeb12485faeafbfd18f1bbeb6f36d2340c3
Rishu <rs139323@gmail.com>
2026-01-28 17:02:57 +0530

    gg

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.post("/register", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 );
